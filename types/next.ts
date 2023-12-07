06b54be2d0b246b9faf1fd0dbc39b590bf34c1f6
熊宁远 <xny01483622@alibaba-inc.com>
2023-12-07 17:19:06 +0800

    fix: remove ws type

--- types/next.ts
+++ types/next.ts
@@ -1,11 +1,2 @@
 import { Server as NetServer, Socket } from "net";
 import { NextApiResponse } from "next";
-import { Server as SocketIOServer } from "Socket.IO";
-
-export type NextApiResponseServerIO = NextApiResponse & {
-    socket: Socket & {
-        server: NetServer & {
-            io: SocketIOServer;
-        };
-    };
-};
