ea5dcf514fec9825ceb049e62c7bf7b876908cdc
xny <985049549@qq.com>
2024-09-03 23:32:00 +0800

    fix: 引用

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import React from "react";
 
 import utilStyles from "../styles/utils.module.css";
 import styles from "./layout.module.css";
